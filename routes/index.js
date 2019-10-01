c6cd78ad527caa713e6a59a06773718df0844771
Aishwarya <aoza@horizon.csueastbay.edu>
2019-09-30 21:27:34 -0700

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,42 @@
-import * as mongodb from "mongodb";
-
 var express = require('express');
+var mongodb = require('mongodb');
+var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSUEB' });
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
-mongodb.get('/mongodb', function (request, response) {
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-  mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+
+app.get('/', function(req, res, next) {
+  res.render('pages/index', { title: 'Express' });
+});
+
+app.get('/mongodb', function(req, res, next) {
+  mongodb.MongoClient.connect('mongodb://Aishwarya:abc123@ds227808.mlab.com:27808/heroku_gb8t8mb3', function(err, client) {
     if(err) throw err;
-    //get collection of user
+    var db = client.db('heroku_gb8t8mb3\n');
     var User = db.collection('User');
-    //get all Routes with frequency >=1
+    useNewUrlParser: true
     User.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
       if(err) throw err;
-
-      response.render('pages/mongodb', {results: docs});
-
+      res.render('pages/mongodb', {results: docs});
     });
-
-    //close connection when your app is terminating.
-    db.close(function (err) {
-      if(err) throw err;
+    client.close(function (err) {
+     if(err) throw err;
     });
   });//end of connect
 });//end app.get
-
-module.exports = router;
+app.listen(PORT, function() {
+  console.log(`Listening on Port ${PORT}`);
+});
